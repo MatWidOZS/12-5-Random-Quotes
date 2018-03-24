35b59e410fd94add00fa60da9bbf87bb5768e81e
Mateusz Widerski <mat.widerski@gmail.com>
2018-03-24 12:50:54 +0100

    Add random quote script

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 $(function() {
 
-	var url = 'http://api.icndb.com/jokes/random';
+	var tweetLink = "https://twitter.com/intent/tweet?text=",
+		quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
-	var $button = $('#get-joke').click(function() {
-		getJoke();
-	});
-
-	var $paragraph = $('#joke');
-
-	function getJoke() {
-		$.ajax({
-			method: 'GET',
-			url: url,	//name of the parameter: name of the variable that holds the value
-			success: function(res) {
-				$paragraph.text(res.value.joke);
-			}
-		});
+	function getQuote() {
+		$.getJSON(quoteUrl, createTweet);
 	}
 
-	getJoke();
+	function createTweet(input) {
+		var data = input[0];
+
+		var quoteText = $(data.content).text().trim();
+		var quoteAuthor = data.title;
+
+		if (!quoteAuthor.length) {
+			quoteAuthor = "Unknown author";
+		}
+
+		var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+		if(tweetText.length > 140) {
+			getQuote();
+		} else {
+			var tweet = tweetLink + encodeURIComponent(tweetText);
+			$('.quote').text(quoteText);
+			$('.author').text('Author: ' + quoteAuthor);
+			$('.tweet').attr('href', tweet);
+		}
+	}
+
+	$(document).ready(function() {
+		getQuote();
+		$('.trigger').click(function() {
+			getQuote();
+		});
+	});
 
 });
